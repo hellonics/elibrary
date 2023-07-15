46bdec793a47b04f9ea1c517a17473c9911eaf7b
giovernando <giovernando24@gmail.com>
2023-07-15 15:33:12 +0700

    tambah route dashboard

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -18,4 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', [DashboardController::class, 'index']);
