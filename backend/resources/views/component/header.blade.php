3b0c6b50185d4f77fc1c2e879d2a687fe401935e
dinduh <dnbigboss@gmail.com>
2023-07-22 15:41:35 +0700

    edit

--- backend/resources/views/component/header.blade.php
+++ backend/resources/views/component/header.blade.php
@@ -1,58 +1,58 @@
  <!-- ======= Header ======= -->
  <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
-            <img src="{{ asset('assets/img/telnest.png') }}" width="100px" alt="">
-        </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- End Logo -->
+     <div class="d-flex align-items-center justify-content-between">
+         <a href="index.html" class="logo d-flex align-items-center">
+             <img src="{{ asset('assets/img/telnest.png') }}" width="100px" alt="">
+         </a>
+         <i class="bi bi-list toggle-sidebar-btn"></i>
+     </div><!-- End Logo -->
 
-    <div class="search-bar">
-        <form class="search-form d-flex align-items-center" method="POST" action="#">
-            <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-            <button type="submit" title="Search"><i class="bi bi-search"></i></button>
-        </form>
-    </div><!-- End Search Bar -->
+     <div class="search-bar">
+         <form class="search-form d-flex align-items-center" method="POST" action="#">
+             <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+             <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+         </form>
+     </div><!-- End Search Bar -->
 
-    <nav class="header-nav ms-auto">
-        <ul class="d-flex align-items-center">
+     <nav class="header-nav ms-auto">
+         <ul class="d-flex align-items-center">
 
-            <li class="nav-item d-block d-lg-none">
-                <a class="nav-link nav-icon search-bar-toggle " href="#">
-                    <i class="bi bi-search"></i>
-                </a>
-            </li><!-- End Search Icon-->
+             <li class="nav-item d-block d-lg-none">
+                 <a class="nav-link nav-icon search-bar-toggle " href="#">
+                     <i class="bi bi-search"></i>
+                 </a>
+             </li><!-- End Search Icon-->
 
-            <li class="nav-item dropdown pe-3">
+             <li class="nav-item dropdown pe-3">
 
-                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
-                    data-bs-toggle="dropdown">
-                    <img src="assets/img/fotonino.jpeg" alt="Profile" class="rounded-circle">
-                    <span class="d-none d-md-block dropdown-toggle ps-2">{{ auth()->user()->name }}</span>
-                </a><!-- End Profile Iamge Icon -->
+                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
+                     data-bs-toggle="dropdown">
+                     <img src="assets/img/fotonino.jpeg" alt="Profile" class="rounded-circle">
+                     <span class="d-none d-md-block dropdown-toggle ps-2"></span>
+                 </a><!-- End Profile Iamge Icon -->
 
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                    <li class="dropdown-header">
-                        <h6>{{ auth()->user()->name }}</h6>
-                        <span>Web Designer</span>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
+                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                     <li class="dropdown-header">
+                         <h6></h6>
+                         <span>Web Designer</span>
+                     </li>
+                     <li>
+                         <hr class="dropdown-divider">
+                     </li>
 
 
-                    <li>
-                        <a class="dropdown-item d-flex align-items-center" href="/logout">
-                            <i class="bi bi-box-arrow-right"></i>
-                            <span>Sign Out</span>
-                        </a>
-                    </li>
+                     <li>
+                         <a class="dropdown-item d-flex align-items-center" href="/logout">
+                             <i class="bi bi-box-arrow-right"></i>
+                             <span>Sign Out</span>
+                         </a>
+                     </li>
 
-                </ul><!-- End Profile Dropdown Items -->
-            </li><!-- End Profile Nav -->
+                 </ul><!-- End Profile Dropdown Items -->
+             </li><!-- End Profile Nav -->
 
-        </ul>
-    </nav><!-- End Icons Navigation -->
+         </ul>
+     </nav><!-- End Icons Navigation -->
 
-</header><!-- End Header -->
+ </header><!-- End Header -->
