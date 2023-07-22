364cda1acfc64b6ccf23527f519542953d4bd48c
dinduh <dnbigboss@gmail.com>
2023-07-22 14:47:13 +0700

    edit tampilan

---
+++ backend/resources/views/component/sidebar.blade.php
@@ -0,0 +1,28 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link  " href="{{ url('/') }}">
+                <i class="bi bi-grid"></i>
+                <span>Home</span>
+            </a>
+        </li><!-- End home Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('order') }}">
+                <i class="bi bi-menu-button-wide"></i><span>Order</span>
+            </a>
+        </li><!-- End orders Nav -->
+
+        <li class="nav-heding">Master Data</li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/daftarbuku') }}">
+                <i class="bi bi-menu-button-wide"></i><span>Daftar Buku</span>
+            </a>
+        </li><!-- End orders Nav -->
+    </ul>
+
+</aside><!-- End Sidebar-->
