8d40757600610414614269710b50c17f5476a04d
rizkyr99 <ramarizdev@gmail.com>
2023-07-23 15:50:53 +0700

    fix: tambah buku view

--- backend/resources/views/buku/create.blade.php
+++ backend/resources/views/buku/create.blade.php
@@ -1,49 +1,49 @@
 @extends('app')
 
-@section('title', 'bank -talnest')
+@section('title', 'Tambah Buku')
 
 @section('main')
-    <main id="main" class="main">
+<main id="main" class="main">
 
-        <div class="pagetitle">
-            <h1>bank Tables</h1>
-            <nav>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
-                    <li class="breadcrumb-item"><a href="{{ url('/bank') }}">bank</a></li>
-                    <li class="breadcrumb-item">tambah</li>
+    <div class="pagetitle">
+        <h1>Tambah Buku</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('/bank') }}">Buku</a></li>
+                <li class="breadcrumb-item">tambah</li>
 
-                </ol>
-            </nav>
-        </div><!-- End Page Title -->
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
 
-        <section class="section">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <form class='row g-3' action="{{ url('/bank/store') }}" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <div class="col-12">
-                                    <label for="name" class="form-label">name</label>
-                                    <input type="text" name="name" id="name" class="form-control">
-                                </div>
-                                <div class="col-12">
-                                    <label for="rek" class="form-label">rek</label>
-                                    <input type="text" name="rek" id="rek" class="form-control">
-                                </div>
-                                <div class="col-12">
-                                    <label for="logo" class="form-label">logo</label>
-                                    <input type="file" name="logo" id="logo" class="form-control"> 
-                                </div>
-                                <div class="col-12">
-                                    <button type="submit" class="btn btn-primary btn-pill">save</button>
-                                </div>
-                            </form>
-                        </div>
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form class='row g-3' action="{{ url('/buku/store') }}" method="POST">
+                            @csrf
+                            <div class="col-12">
+                                <label for="judul" class="form-label">Judul</label>
+                                <input type="text" name="judul" id="judul" class="form-control">
+                            </div>
+                            <div class="col-12">
+                                <label for="penulis" class="form-label">Penulis</label>
+                                <input type="text" name="penulis" id="penulis" class="form-control">
+                            </div>
+                            <div class="col-12">
+                                <label for="kategori" class="form-label">Kategori</label>
+                                <input type="text" name="kategori" id="kategori" class="form-control">
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary btn-pill">save</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </section>
-    </main>
+        </div>
+    </section>
+</main>
 @endsection
